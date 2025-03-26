e944f80b13f3e054590ac41a2e87e7cf1434fd1b
mxkcpy <mikolaj19@poczta.fm>
2025-03-26 18:18:38 +0100

    added initial input processing

---
+++ processInput.py
@@ -0,0 +1,19 @@
+import sys
+import re
+
+
+def checkProgramCall():
+    if len(sys.argv) != 6:
+        print(
+            "Usage: python fifteenPuzzle.py <bfs/dfs/astr> <permutations of LURD/hamm/manh> <puzzleFileName> <outputFileName> <specificOutputFileName"
+        )
+        sys.exit(1)
+    if re.match("bfs|dfs|astr", sys.argv[1]) is None:
+        print("Usage: python fifteenPuzzle.py <bfs/dfs/astr>**")
+    if re.match("[LURD]{4}|manh|hamm", sys.argv[2]) is None:
+        print(
+            "Usage: python fifteenPuzzle.py <bfs/dfs/astr> <permutations of LURD/hamm/manh>**"
+        )
+
+
+checkProgramCall()
--- steps.md
+++ steps.md
@@ -10,8 +10,8 @@
 
 1. the program requires **5** command line arguments
 
-1.1 the program strategy (bfs, dfs, astr)
-1.2 the strategy options (permutations of LURD, hamm, manh)
-1.3 text-file name with the initial puzzle
-1.4 solution text-file
+1.1 the program strategy (bfs, dfs, astr) \
+1.2 the strategy options (permutations of LURD, hamm, manh) \
+1.3 text-file name with the initial puzzle \
+1.4 solution text-file \
 1.5 text-file with additional solution information
