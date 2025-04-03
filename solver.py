1de966eaf42ba36c4ecc3c9dd8b90d5128fc792d
251643 Emilia Szczerba <251643@edu.p.lodz.pl>
2025-04-03 20:52:31 +0200

    Puzzle class, solvedState, possibleMoves

--- solver.py
+++ solver.py
@@ -1,11 +1,26 @@
 from processInput import checkProgramCall
 import sys
-
+import copy
+from typing import List, Tuple, Optional, Set, Dict, Callable, Union
 
 class Puzzle:
-    board = []
-    rowNr = 0
-    colNr = 0
+    def __init__(self, filename: Optional[str] = None, rows: int = 4, cols: int = 4) -> None:
+
+        self.visitedStates: int = 0
+        self.processedStates: int = 0
+        self.max_recursionDepth: int = 0
+        self.solutionLength: int = 0
+        self.computationTime: float = 0.0
+
+        self.board: List[List[int]] = []
+        self.zeroPos: Tuple[int, int] = (0, 0) #pozycja pustego pola (wiersz, kolumna)
+        self.rowNr: int = rows
+        self.colNr: int = cols
+
+        if filename:
+            self.read(self, filename)
+        else:
+            self.initSolvedState(rows, cols)
 
     def read(self, fileName: str):
         with open(fileName) as f:
@@ -21,6 +36,50 @@ class Puzzle:
                 elementVal = list(map(int, elements))
                 self.board.append(elementVal)
                 elements = f.readline().strip().replace(" ", "")
+
+    def initSolvedState(self, rowNr: int, colNr: int) -> None: # Inicjalizacja w stanie rozwiązanym
+        self.board = []
+        count = 1
+        for i in range(rowNr):
+            row = []
+            for j in range(colNr):
+                if i == rowNr - 1 and j == colNr - 1:
+                    row.append(0)
+                    self.zeroPos = (i, j)
+                else:
+                    row.append(count)
+                    count += 1
+            self.board.append(row)
+
+    def isGoal(self) -> bool: # Sprawdza czy plansza jest w stanie końcowym
+        count = 1
+        for i in range(self.rowNr):
+            for j in range(self.colNr):
+                if i == self.rowNr - 1 and j == self.colNr - 1:
+                    if self.board[i][j] != 0:
+                        return False
+                elif self.board[i][j] != count:
+                    return False
+                count += 1
+        return True
+
+    def getPossibleMoves(self) -> List[str]: # Zwraca możliwe ruchy z aktualnego stany planszy
+        moves = []
+        row, col = self.zeroPos
+
+        if col > 0:
+            moves.append('L')
+
+        if col < self.colNr - 1:
+            moves.append('R')
+
+        if row > 0:
+            moves.append('U')
+
+        if row < self.rowNr - 1:
+            moves.append('D')
+
+        return moves
 
 
 class Solver:
